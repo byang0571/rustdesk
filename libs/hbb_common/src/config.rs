d65b412879126b3be8136e1207bc2c86f9906719
byang <139646837+byang0571@users.noreply.github.com>
2024-09-29 20:57:39 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["119.29.84.34"];
-pub const PUBLIC_RS_PUB_KEY: &str = "hHEfn67vLRu2Isx33ieLvGokZTA7wUZN+M6f0DuwPK4";
+pub const PUBLIC_RS_PUB_KEY: &str = "hHEfn67vLRu2Isx33ieLvGokZTA7wUZN+M6f0DuwPK4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
